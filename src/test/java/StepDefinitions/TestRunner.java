291c293d2f142fff0dc4f5812e26861a9a468543
Prasanga Fernando <prasangafdz@outlook.com>
2021-07-14 09:29:30 +0530

    Added Cucumber Junit dependency

--- src/test/java/StepDefinitions/TestRunner.java
+++ src/test/java/StepDefinitions/TestRunner.java
@@ -1,5 +1,12 @@
 package StepDefinitions;
 
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features="")
 public class TestRunner {
 
 }
